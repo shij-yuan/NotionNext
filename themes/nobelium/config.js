df5d8d223ab0061de12836b92fcca124f5cf4e18
shijia.yuan <shijia.yuan@shopee.com>
2024-08-09 18:50:40 +0800

    config

--- themes/nobelium/config.js
+++ themes/nobelium/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
   NOBELIUM_MENU_TAG: true, // 显示标签
   NOBELIUM_MENU_ARCHIVE: false, // 显示归档
   NOBELIUM_MENU_SEARCH: true, // 显示搜索
-  NOBELIUM_MENU_RSS: false, // 显示订阅
+  NOBELIUM_MENU_RSS: true, // 显示订阅
 
   NOBELIUM_AUTO_COLLAPSE_NAV_BAR: true // 页头导航栏动画
 }
